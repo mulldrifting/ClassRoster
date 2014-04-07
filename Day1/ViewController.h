2b30ed2310fa5c131e143ca6fb00b45c90b502f9
Lauren Lee <unrealeel@gmail.com>
2014-04-07 11:18:19 -0700

    Initial Commit

---
+++ Day1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Day1
+//
+//  Created by Lauren Lee on 4/7/14.
+//  Copyright (c) 2014 Lauren Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Day1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Day1
+//
+//  Created by Lauren Lee on 4/7/14.
+//  Copyright (c) 2014 Lauren Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
